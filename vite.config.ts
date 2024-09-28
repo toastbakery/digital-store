eac0b579000888c6c8c4bdbfe68f6824eb9a9d3c
fermentday <348207971@qq.com>
2024-09-28 14:49:20 +0100

    modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/digital-store/",
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    base: command === "build" ? "/digital-store/" : "/", // 区分开发和生产
+  };
 });
