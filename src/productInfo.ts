e2229646b325c53e825b77182f7456f4bc5ff9df
fermentday <348207971@qq.com>
2024-09-29 17:41:47 +0100

    modify image path again

--- src/productInfo.ts
+++ src/productInfo.ts
@@ -1,11 +1,14 @@
 import { Product } from "./types";
 
+const basePath =
+  import.meta.env.MODE === "production" ? "/digital-store" : "./src";
+
 const PRODUCT: Product = {
   id: 1,
   title: "Complete Web Development Bundle",
   description:
     "Learn to build websites with HTML, CSS, Javascript, React, Node, and Mongo",
-  image: "/src/assets/thumbnail.png",
+  image: `${basePath}/assets/thumbnail.png`,
   price: 12.99,
 };
 export default PRODUCT;
